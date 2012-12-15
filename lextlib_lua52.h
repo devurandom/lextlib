06d51f2a262e75b650ce2f1ef0a52c53951e5344
Dennis Schridde <devurandom@gmx.net>
2012-12-15 21:09:48 +0100

    Improved readability and comments for Lua 5.2 fallbacks

--- lextlib_lua52.h
+++ lextlib_lua52.h
@@ -9,21 +9,30 @@
 
 
 #if LUA_VERSION_NUM < 502
+/* Error codes for lua_load: */
 #	define LUA_OK 0
 #	define LUA_ERRGCMM (-1)
+
+/* Comparison types for lua_compare: */
 #	define LUA_OPEQ 0
 #	define LUA_OPLT 1
 #	define LUA_OPLE 2
-/* lua_rawlen: Not entirely correct, but should work anyway */
+
+/* WARNING: Not entirely correct, but should work anyway */
 #	define lua_rawlen lua_objlen
-/* lua_...uservalue: Something very different, but it should get the job done */
+
+/* WARNING: Something very different, but it might get your job done */
 #	define lua_getuservalue lua_getfenv
 #	define lua_setuservalue lua_setfenv
+
 #	define lua_tonumberx(L,i,b) (lua_isnumber(L,(i)) ? (*(b)=1, lua_tonumber(L,(i))) : (*(b)=0, 0))
 #	define lua_tointegerx(L,i,b) (lua_isnumber(L,(i)) ? (*(b)=1, lua_tointeger(L,(i))) : (*(b)=0, 0))
+
 #	define luaL_newlib(L,l) (lua_newtable(L), luaL_register(L,NULL,l))
 #	define luaL_setfuncs(L,l,n) (assert(n==0), luaL_register(L,NULL,l))
+
 #	define luaL_setmetatable(L,t) (luaL_getmetatable(L,t), lua_setmetatable(L,-2))
+
 #	define luaL_testudata(L,i,t) ( \
 	lua_getmetatable(L,(i)), \
 	!lua_isnil(L,-1) ? ( \
@@ -45,6 +54,7 @@
 		NULL \
 	) \
 )
+
 #	define lua_compare(L,i1,i2,op) ( \
 	(op) == LUA_OPEQ ? ( \
 		lua_equal(L,(i1),(i2)) \
